b007b47f2f91d94920ba885db42a9f09c222a041
oyewosolomon <oyewosolomon@gmail.com>
2025-03-26 09:25:57 +0100

    Updated fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'], // Make Open Sans default
+        system: ['ui-sans-serif', 'system-ui', 'sans-serif'], // Fallback
+      },
       animation: {
         'fade-in': 'fadeIn 1s ease-in-out',
         'fade-in-up': 'fadeInUp 1s ease-in-out',
